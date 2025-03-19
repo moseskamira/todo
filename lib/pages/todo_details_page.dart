b5b40cf483ca428cec441d3a55d781aa5b01fd4c
moseskamira <moses.african@gmail.com>
2025-03-19 20:20:30 +0300

    Improve Edit Todo

--- lib/pages/todo_details_page.dart
+++ lib/pages/todo_details_page.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:todo/models/todo_model.dart';
+import 'package:provider/provider.dart';
 import 'package:todo/routes/route_name.dart';
+import 'package:todo/view_model/todos_provider.dart';
 
 class TodoDetailsPage extends StatefulWidget {
-  final TodoModel todo;
   final int index;
 
   const TodoDetailsPage({
     super.key,
-    required this.todo,
     required this.index,
   });
 
@@ -43,6 +42,7 @@ Widget _buildDetailRow(String label, String value) {
 class _TodoDetailsPageState extends State<TodoDetailsPage> {
   @override
   Widget build(BuildContext context) {
+    final todoProvider = Provider.of<TodosProvider>(context, listen: true);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -56,40 +56,55 @@ class _TodoDetailsPageState extends State<TodoDetailsPage> {
         padding: const EdgeInsets.all(16.0),
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              _buildDetailRow('Title', widget.todo.title ?? 'No Title'),
-              _buildDetailRow(
-                  'Description', widget.todo.description ?? 'No Description'),
-              _buildDetailRow('Status', widget.todo.status ?? 'Unknown'),
-              const SizedBox(height: 30),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton.icon(
-                  onPressed: () {
-                    Navigator.of(context).pushNamed(
-                      RouteName.updateTodoScreen,
-                      arguments: {'todo': widget.todo, 'index': widget.index},
-                    );
-                  },
-                  icon: const Icon(Icons.edit, color: Colors.white),
-                  label: Text(
-                    'Edit Todo',
-                    style: GoogleFonts.poppins(
-                        fontSize: 16, fontWeight: FontWeight.w500),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    backgroundColor: Colors.blue,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+          child: FutureBuilder(
+              future: todoProvider.getDetails(widget.index),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (snapshot.hasError) {
+                  return Text(snapshot.error.toString());
+                }
+                if (!snapshot.hasData) return Text('Failed to fetch Todo');
+                final todo = snapshot.data!;
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    _buildDetailRow('Title', todo.title ?? 'No Title'),
+                    _buildDetailRow(
+                        'Description', todo.description ?? 'No Description'),
+                    _buildDetailRow('Status', todo.status ?? 'Unknown'),
+                    const SizedBox(height: 30),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton.icon(
+                        onPressed: () {
+                          Navigator.of(context).pushNamed(
+                            RouteName.updateTodoScreen,
+                            arguments: {'todo': todo, 'index': widget.index},
+                          );
+                        },
+                        icon: const Icon(Icons.edit, color: Colors.white),
+                        label: Text(
+                          'Edit Todo',
+                          style: GoogleFonts.poppins(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.white,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 12),
+                          backgroundColor: Colors.blue,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+                  ],
+                );
+              }),
         ),
       ),
     );
